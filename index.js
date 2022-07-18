388b3e3e70e3632399773754367d9d3a81ccac69
nileshverma10 <100075212+nileshverma10@users.noreply.github.com>
2022-07-18 13:18:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+var s=2;
+var b=4;
+var c=s+b;
+console.log(c);
+
+var s=2;
+var b=4;
+var c=s-b;
+console.log(c);
+
+
+var s=2;
+var b=4;
+var c=s*b;
+console.log(c);
+
+
+var s=2;
+var b=4;
+var c=s/b;
+console.log(c);
+
+var s=2;
+var b=4;
+var c=s%b;
+console.log(c);
+
+
+var sw=23;
+var pw=45;
+if(sw>pw){
+    console.log("sw is greater");
+}
+
+else("pw is smallwer")
+
